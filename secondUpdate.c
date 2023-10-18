87a7056ea19a18ebdef500d1f422541ac94e11d5
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-10-18 18:58:23 -0300

    Update thirdUpdate.c

--- secondUpdate.c
+++ secondUpdate.c
@@ -24,27 +24,35 @@ typedef struct
 } node;
 
 // Function prototypes
-void add_non_directed_edge(node *nd, int **matrix);
-void remove_non_directed_edge(node *nd, int **matrix);
-void add_directed_edge(node *nd, int **matrix);
-void remove_directed_edge(node *nd, int **matrix);
 void create_graph_list(Grafo *g, int n);
 void delete_graph_list(Grafo *g);
 void add_edge_list(Grafo *g, int v1, int v2);
 int has_edge_list(Grafo *g, int v1, int v2);
 void remove_edge_list(Grafo *g, int v1, int v2);
 void print_graph_list(Grafo *gr);
+void add_non_directed_edge(node *nd, int **matrix);
+void remove_non_directed_edge(node *nd, int **matrix);
+void add_directed_edge(node *nd, int **matrix);
+void remove_directed_edge(node *nd, int **matrix);
+void consult_node_degree(node *nd);
+void consult_graph_degree(node *nd);
+void consult_node_neighbourhood(node *nd);
+int is_graph_connected();
+int is_graph_regular(node *nd);
+int is_graph_complete();
+void is_nodes_connected();
 
 Grafo g; // Graph g global variable
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//
 void create_graph_list(Grafo *g, int n) {
   g->lista = calloc(n, sizeof(No *));
   g->n = n;
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
+//
 void delete_graph_list(Grafo *g) {
   for (int i = 0; i < g->n; i++) {
     No *l = g->lista[i];
@@ -58,7 +66,6 @@ void delete_graph_list(Grafo *g) {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 // Adicionando aresta na lista de adjacência de v1
 void add_edge_list(Grafo *g, int v1, int v2) {
   No **a = &g->lista[v1];
@@ -76,7 +83,6 @@ void add_edge_list(Grafo *g, int v1, int v2) {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 // Retorna True (1) ou False (0) caso exista uma aresta entre v1 e v2
 int has_edge_list(Grafo *g, int v1, int v2) {
   No *l = g->lista[v1];
@@ -117,7 +123,7 @@ void print_graph_list(Grafo *gr) {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//
+//Add edges for directed graphs
 void add_non_directed_edge (node *nd, int **matrix){
 
     int n1, n2, weight, flag = 0;
@@ -165,113 +171,195 @@ void add_non_directed_edge (node *nd, int **matrix){
 
     matrix[n1][n2] = weight;
     matrix[n2][n1] = weight;
-  
-    add_edge_list(&g, n1, n2);
-    add_edge_list(&g, n2, n1);
+
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//
+//Remove edges for non directed graphs
 void remove_non_directed_edge (node *nd, int **matrix){
-  int n1, n2;
+    
+    int n1, n2;
 
-  printf("Digite um vertice a ser removido: ");
-  scanf("%d", &n1);
+    printf("Digite um vertice a ser removido: ");
+    scanf("%d", &n1);
 
-  printf("Digite outro vertice a ser removido junto com o anterior: ");
-  scanf("%d", &n2);
+    printf("Digite outro vertice a ser removido junto com o anterior: ");
+    scanf("%d", &n2);
 
-  for (int i = 0; i < nd[n1].degree; i++){
+    for (int i = 0; i < nd[n1].degree; i++){
 
-      if (nd[n1].node_adj[i] == n2){
-          nd[n1].node_adj[i] = -1;
-          break;
-      }
-  }
+        if (nd[n1].node_adj[i] == n2){
+            nd[n1].node_adj[i] = -1;
+            break;
+        }
+    }
 
-  nd[n1].degree--;
+    nd[n1].degree--;
 
-  for (int i = 0; i < nd[n2].degree; i++){
+    for (int i = 0; i < nd[n2].degree; i++){
 
-      if (nd[n2].node_adj[i] == n1){
-          nd[n2].node_adj[i] = -1;
-          break;
-      }
-  }
+        if (nd[n2].node_adj[i] == n1){
+            nd[n2].node_adj[i] = -1;
+            break;
+        }
+    }
 
-  nd[n2].degree--;
+    nd[n2].degree--;
 
-  matrix[n1][n2] = 0;
-  matrix[n2][n1] = 0;
-  
-  remove_edge_list(&g, n1, n2);
-  remove_edge_list(&g, n2, n1);
+    matrix[n1][n2] = 0;
+    matrix[n2][n1] = 0;
+
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//
+//Add edges for directed graphs
 void add_directed_edge(node *nd, int **matrix){
 
-  int n1, n2, weight, flag;
+    int n1, n2, weight, flag;
 
-  printf("Digite o vertice de saida: ");
-  scanf("%d", &n1);
+    printf("Digite o vertice de saida: ");
+    scanf("%d", &n1);
 
-  printf("Digite o vertice de chegada: ");
-  scanf("%d", &n2);
+    printf("Digite o vertice de chegada: ");
+    scanf("%d", &n2);
 
-  printf("Digite o peso da aresta adicionada (Digite 1 caso ela nao seja ponderada): ");
-  scanf("%d", &weight);
+    printf("Digite o peso da aresta adicionada (Digite 1 caso ela nao seja ponderada): ");
+    scanf("%d", &weight);
 
-  for (int i = 0; i < nd[n1].degree; i++){
+    for (int i = 0; i < nd[n1].degree; i++){
 
-      if (nd[n1].node_adj[i] == -1){
-          nd[n1].node_adj[i] = n2;
-          flag = 1;
-          break;
-      } 
-  }
+        if (nd[n1].node_adj[i] == -1){
+            nd[n1].node_adj[i] = n2;
+            flag = 1;
+            break;
+        } 
+    }
 
-  if (flag == 0){
-      nd[n1].node_adj[nd[n1].degree] = n2;
-  }
+    if (flag == 0){
+        nd[n1].node_adj[nd[n1].degree] = n2;
+    }
 
-  flag = 0;
+    flag = 0;
 
-  nd[n1].degree++;
-  matrix[n1][n2] = weight;
-  
-  add_edge_list(&g, n1, n2);
+    nd[n1].degree++;
+    matrix[n1][n2] = weight;
+
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//
+//Remove edges for directed graphs
 void remove_directed_edge (node *nd, int **matrix){
 
-  int n1, n2;
+    int n1, n2;
 
-  printf("Digite o vertice de saida: ");
-  scanf("%d", &n1);
+    printf("Digite o vertice de saida: ");
+    scanf("%d", &n1);
 
-  printf("Digite um vértice de chegada alcancavel pelo vertice anteriormente digitado: ");
-  scanf("%d", &n2);
+    printf("Digite um vértice de chegada alcancavel pelo vertice anteriormente digitado: ");
+    scanf("%d", &n2);
 
-  if (matrix[n1][n2] != 0){
+    if (matrix[n1][n2] != 0){
 
-      for (int i = 0; i < nd[n1].degree; i++){
+        for (int i = 0; i < nd[n1].degree; i++){
 
-          if (nd[n1].node_adj[i] == n2){
-              nd[n1].node_adj[i] = -1;
-              break;
-          }
-      }
+            if (nd[n1].node_adj[i] == n2){
+                nd[n1].node_adj[i] = -1;
+                break;
+            }
+        }
 
-      nd[n1].degree--;
+        nd[n1].degree--;
 
-      matrix[n1][n2] = 0;
-    
-      remove_edge_list(&g, n1, n2);
-  } else printf("Vertice nao diretamente alcançavel (não são vizinhos).");
+        matrix[n1][n2] = 0;
+
+    } else
+        printf("Vertice nao alcancavel.");
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Consult node degree
+void consult_node_degree(node *nd){
+
+    int n;
+
+    printf("Digite o vertice que deseja consultar: ");
+    scanf("%d", &n);
+
+    printf("Grau do vertice %d: %d", n, nd[n].degree);
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Consult graph degree
+void consult_graph_degree(node *nd){
+
+    int len, g_degree;
+
+    len = sizeof(nd) / sizeof(node);  //Substituir
+
+    for (int i = 0; i < len; i++){
+
+        g_degree += nd[i].degree;
+        
+    }
+
+    printf("Grau do grafo: %d", g_degree);
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//consult node neighbourhood
+void consult_node_neighbourhood(node *nd){
+
+    int n;
+
+    printf("Digite o vertice para exibir seus vizinhos: ");
+    scanf("%d", n);
+
+    printf("Adjacentes de %d: ", n);
+
+    for (int i = 0; i < nd[n].degree; i++)
+        printf("\t%d", nd[n].node_adj[i]);
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Verify if this graph is connected
+int is_graph_connected(){
+    return 0;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Verify if this graph is regular
+int is_graph_regular(node *nd){
+
+    int len, comp;
+
+    comp = nd[0].degree;
+    len = sizeof(nd) / sizeof(node);  //Substituir
+
+    for (int i = 1; i < len; i++){
+
+        if (nd[i].degree == comp){
+            continue;
+        } else return 0;
+
+    }
+
+    return 1;
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Verify if this graph is complete
+int is_graph_complete(){
+    return 0;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//Verify if exists connections between two nodes
+void is_nodes_connected(){
 
 }
 
